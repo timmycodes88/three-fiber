e9ac040f55bf5dc1595767f53f6e682f2cb65874
Tim <timvan0118@gmail.com>
2022-12-16 22:38:27 -0600

    Works Now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg"
-import "./App.css"
 import { Canvas } from "@react-three/fiber"
 
 function App() {
   return (
     <Canvas>
-      <mesh></mesh>
+      <mesh>
+        <boxGeometry args={[10, 10, 10]} />
+        <meshStandardMaterial />
+      </mesh>
     </Canvas>
   )
 }
